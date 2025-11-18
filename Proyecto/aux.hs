9afb5d1dbf1bc7f4c1c1b1ccd63dc16dc2c34041
natdom07-cmyk <natdom07@ciencias.unam.mx>
2025-11-18 13:16:16 -0600

    Ya resolvi las dudas de como poder implementar los arboles, me base en las funciones vistas en la practica, y note mi error ya que intente programar los arboles de huffman, como en el video y la pagina proporcionadas pro el profesor. En este caso ya entendi que es programar los arboles. Añadi las funcione para medir frecuencia, profundidad y mas funciones auxiliares que van a servir para la creacion de los arboles. En este caso, Añadi en los comentarios, una pequeña nota de desarrollo

--- Proyecto/aux.hs
+++ Proyecto/aux.hs
@@ -33,10 +33,9 @@ reversa (x:xs) = concatenar (reversa xs) [x]
 {- 
 Tenemos que definir la estructura de un arbol binario, esta misma la podemos sacar como se definieron los arboles binarios
 en nuestra practica 6, usamos los metodos que nos proporciono el profesor
--}
-
 data Arbol a = Vacio | AB a (Arbol a) (Arbol a) 
   deriving (Eq, Ord, Show)
+-}
 
 {-
  - Idea para definir un arbol binario de huffman, crear una funcion en la cual podamos crear Nodos, de huffman
@@ -50,6 +49,72 @@ Posible estructura de lo anterior
 data NodoHuffman = Hoja Char Int | Nodo Int  --Esto lo ponemos como una manera de poder definir lo de la idea
     deriving(Eq, Show) -- Esto lo añadimos para poder imprimir y hacer una comparacion
 
+
+## Notas de Desarrollo
+
+### Corrección de Enfoque 
+Inicialmente comence a implementar el algoritmo de Huffman clásico de la manera en la cual se vizualisa en el video de apoyo
+proporcionado por el profe, y como se mostraba en la pagina de constructor de arboles de huffman, igual dada por el profe 
+pero el proyecto requiere una variación de lo de huffman donde:
+- Se construye de ARRIBA hacia ABAJO
+- Se inserta en orden descendente de frecuencia
+- El símbolo de mayor frecuencia va a la DERECHA
+- Se expande siempre la hoja más a la izquierda y profunda
  -}
 
- 
+ data NodoHuffman = Hoja Char Int | Nodo Int
+    deriving (Eq, Show)
+
+{-
+ - ArbolHuffman: Tipo del árbol de Huffman usando la estructura de Arbol de la Práctica 6
+ -}
+type ArbolHuffman = Arbol NodoHuffman
+
+{-
+ - obtenerFrecuencia: Obtiene la frecuencia de un nodo Huffman
+ -}
+obtenerFrecuencia :: NodoHuffman -> Int
+obtenerFrecuencia (Hoja _ freq) = freq
+obtenerFrecuencia (Nodo freq) = freq
+{-
+ - numElementos: Cuenta el número de nodos en un árbol
+ - Basado en el ejemplo de práctica de árboles binarios
+ -}
+numElementos :: Arbol a -> Int
+numElementos Vacio = 0
+numElementos (AB _ Vacio Vacio) = 1
+numElementos (AB _ izq der) = 1 + numElementos izq + numElementos der
+
+{-
+ - profundidad: Calcula la profundidad de un árbol
+ - Basado en el ejemplo de práctica de árboles binarios
+ -}
+profundidad :: Arbol a -> Int
+profundidad Vacio = 0
+profundidad (AB _ Vacio Vacio) = 1
+profundidad (AB _ izq der) = 1 + max (profundidad izq) (profundidad der)
+
+{-
+ - agruparConsecutivos: Agrupa elementos consecutivos iguales en sublistas
+ - Necesario para calcular frecuencias de caracteres
+ -}
+agruparConsecutivos :: Eq a => [a] -> [[a]]
+agruparConsecutivos [] = []
+agruparConsecutivos (x:xs) = (x : iguales) : agruparConsecutivos diferentes
+  where
+    iguales = tomarMientras (== x) xs
+    diferentes = descartarMientras (== x) xs
+
+{-
+ - tomarMientras: Toma elementos mientras cumplan una condición
+ -}
+tomarMientras :: (a -> Bool) -> [a] -> [a]
+tomarMientras _ [] = []
+tomarMientras pred (x:xs) | pred x    = x : tomarMientras pred xs | otherwise = []
+
+{-
+ - descartarMientras: Descarta elementos mientras cumplan una condición
+ -}
+descartarMientras :: (a -> Bool) -> [a] -> [a]
+descartarMientras _ [] = []
+descartarMientras pred (x:xs) | pred x    = descartarMientras pred xs | otherwise = x:xs
