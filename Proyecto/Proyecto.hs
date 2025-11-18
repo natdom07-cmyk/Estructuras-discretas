31f0352c0476494bf915f9c9aa6b58acc31f7d54
natdom07-cmyk <natdom07@ciencias.unam.mx>
2025-11-18 15:25:46 -0600

    Solucione el error con el ejemplo2, ya que si compilaba como lo deseado pero no regresaba nada en el paso de la decodificacion

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -97,12 +97,11 @@ expandirHojaIzquierda (AB nodo Vacio Vacio) c f =
 expandirHojaIzquierda (AB nodo izq der) c f =
   AB nodo (expandirHojaIzquierda izq c f) der
 
-
 {-
  - generarTablaCodigos: Genera tabla de códigos recorriendo el árbol
  - Asigna '0' a rama izquierda, '1' a rama derecha
  -}
-  generarTablaCodigos :: ArbolHuffman -> [(Char, String)]
+generarTablaCodigos :: ArbolHuffman -> [(Char, String)]
 generarTablaCodigos arbol = generarCodigosAux arbol ""
 
 {-
@@ -155,7 +154,7 @@ codificarSolo texto = textoCodificado
   where
     (textoCodificado, _, _) = codificar texto
 
- {-
+{-
  - decodificar: Convierte binario a texto usando árbol de Huffman
  - Recorre árbol según bits: '0'=izquierda, '1'=derecha
  - Al llegar a hoja: guarda carácter y vuelve a raíz
@@ -182,3 +181,119 @@ decodificarAux arbolOriginal (AB _ izq der) (bit:bits)
   | bit == '1' = decodificarAux arbolOriginal der bits
   | otherwise  = ""
 decodificarAux _ _ [] = ""
+
+{-
+ - mostrarArbol: Muestra el árbol de Huffman de forma legible
+ -}
+mostrarArbol :: ArbolHuffman -> String -> String
+mostrarArbol Vacio indent = concatenar indent "Vacio\n"
+mostrarArbol (AB (Hoja c f) Vacio Vacio) indent = 
+  concatenar indent (concatenar "Hoja: '" (concatenar [c] (concatenar "' (freq: " (concatenar (show f) ")\n"))))
+mostrarArbol (AB (Nodo f) izq der) indent = 
+  let linea1 = concatenar indent (concatenar "Nodo (freq: " (concatenar (show f) ")\n"))
+      subIzq = mostrarArbol izq (concatenar indent "  ")
+      subDer = mostrarArbol der (concatenar indent "  ")
+  in concatenar linea1 (concatenar subIzq subDer)
+
+{-
+ - mostrarTabla: Muestra la tabla de códigos de forma legible
+ -}
+mostrarTabla :: [(Char, String)] -> String
+mostrarTabla [] = ""
+mostrarTabla ((c, codigo):resto) = 
+  concatenar "  '" (concatenar [c] (concatenar "' -> " (concatenar codigo (concatenar "\n" (mostrarTabla resto)))))
+
+{-
+ - calcularEstadisticas: Calcula bits originales y bits comprimidos
+ - Retorna: (bits originales, bits comprimidos)
+ -}
+calcularEstadisticas :: String -> String -> (Int, Int)
+calcularEstadisticas textoOriginal textoCodificado = 
+  (bitsOriginales, bitsComprimidos)
+  where
+    bitsOriginales = longitud textoOriginal * 8
+    bitsComprimidos = longitud textoCodificado
+
+{-
+ - mostrarFrecuencias: Muestra lista de frecuencias
+ -}
+mostrarFrecuencias :: [(Char, Int)] -> String
+mostrarFrecuencias [] = ""
+mostrarFrecuencias ((c, f):resto) = 
+  concatenar "  '" (concatenar [c] (concatenar "' -> " (concatenar (show f) (concatenar "\n" (mostrarFrecuencias resto)))))
+
+{-
+ - ejemplo1: Ejemplo con "Viva el tocho"
+ -}
+ejemplo1 :: IO ()
+ejemplo1 = do
+  let texto = "Viva el tocho"
+  putStrLn ("Ejemplo no.1: \"" ++ texto ++ "\"")
+  
+  putStrLn "1. Frecuencia:"
+  let freqs = calcularFrecuencias texto
+  putStrLn (mostrarFrecuencias freqs)
+  
+  putStrLn "\n2. Arbol de Huffman:"
+  let arbol = construirArbolHuffman texto
+  putStrLn (mostrarArbol arbol "")
+  
+  putStrLn "3. Tabla:"
+  let tabla = generarTablaCodigos arbol
+  putStrLn (mostrarTabla tabla)
+  
+  putStrLn "4. Codificacion:"
+  let (textoCodificado, _, _) = codificar texto
+  putStrLn ("  Binario: " ++ textoCodificado)
+  
+  let (bitsOrig, bitsComp) = calcularEstadisticas texto textoCodificado
+  putStrLn ("  Bits originales : " ++ show bitsOrig)
+  putStrLn ("  Bits comprimidos: " ++ show bitsComp)
+  
+  putStrLn "\n5. Decodificacion:"
+  let textoDecodificado = decodificar arbol textoCodificado
+  putStrLn ("  Texto decodificado: \"" ++ textoDecodificado ++ "\"")
+  putStrLn ("  Coincide: " ++ if texto == textoDecodificado then "SI" else "NO")
+  putStrLn ""
+
+{-
+ - ejemplo2: Ejemplo con otra frase
+ -}
+ejemplo2 :: IO ()
+ejemplo2 = do
+  let texto = "La gente viene y va"
+  putStrLn ("Ejemplo no.1: \"" ++ texto ++ "\"")
+  
+  putStrLn "1. Frecuencia:"
+  let freqs = calcularFrecuencias texto
+  putStrLn (mostrarFrecuencias freqs)
+  
+  putStrLn "\n2. Arbol de Huffman:"
+  let arbol = construirArbolHuffman texto
+  putStrLn (mostrarArbol arbol "")
+  
+  putStrLn "3. Tabla:"
+  let tabla = generarTablaCodigos arbol
+  putStrLn (mostrarTabla tabla)
+  
+  putStrLn "4. Codificacion:"
+  let (textoCodificado, _, _) = codificar texto
+  putStrLn ("  Binario: " ++ textoCodificado)
+  
+  let (bitsOrig, bitsComp) = calcularEstadisticas texto textoCodificado
+  putStrLn ("  Bits originales: " ++ show bitsOrig)
+  putStrLn ("  Bits comprimidos: " ++ show bitsComp)
+  
+  putStrLn "\n5. Decodificacion:"
+  let textoDecodificado = decodificar arbol textoCodificado
+  putStrLn ("  Texto decodificado: \"" ++ textoDecodificado ++ "\"")
+  putStrLn ("  Coincide: " ++ if texto == textoDecodificado then "SI" else "NO")
+  putStrLn ""
+
+{-
+ - ejecutarEjemplos: Ejecuta todos los ejemplos
+ -}
+ejecutarEjemplos :: IO ()
+ejecutarEjemplos = do
+  ejemplo1
+  ejemplo2
